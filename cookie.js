d4988c3f36bc09019fdb116f3082ceafeb3192a9
kthxbyeFOOT <97827302+kthxbyeFOOT@users.noreply.github.com>
2022-05-22 16:57:56 +0530

    Add files via upload

---
+++ cookie.js
@@ -0,0 +1,18 @@
+img = ""
+status = ""
+function preload(){
+img = loadImage('cookiiieieie.jpeg')
+}
+
+function setup()
+{
+canvas = createCanvas(640,420)
+canvas.center()
+//objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+
+function draw()
+{
+image(img, 0, 0, 640, 420)
+}
